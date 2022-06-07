d6dc0b6b8046ad5f1fe50779437b27e819b2b144
bachp <phamthaibach2262@gmail.com>
2022-06-07 07:48:05 +0300

    THe game is now complete!

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let guess;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -26,28 +27,22 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    //Guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      score = score - 1;
-      console.log((document.querySelector('.score').textContent = score));
-      document.querySelector('.message').textContent = 'Too high!';
-    } else {
-      document.querySelector('.message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = '0';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
-    //Guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      score = score - 1;
-      console.log((document.querySelector('.score').textContent = score));
-      document.querySelector('.message').textContent = 'Too low!';
-    } else {
-      document.querySelector('.message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = '0';
-    }
-  }
-});
+
+    //Wrong guess
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        score = score - 1;
+        console.log((document.querySelector('.score').textContent = score));
+        document.querySelector('.message').textContent = guess > secretNumber?'Too high!':"Too low!";
+      } else {
+        document.querySelector('.message').textContent = 'Game over!';
+        document.querySelector('.score').textContent = '0';
+      }}
+    
 
 //Implement again button
 document.querySelector('.again').addEventListener('click', function () {
